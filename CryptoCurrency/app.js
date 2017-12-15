89aabe2161c2f55fa0abf24cab68146973e452d7
Juri Adams <juri@4da.ms>
2017-12-15 15:28:51 +0100

    📚 Added comments to process

--- CryptoCurrency/app.js
+++ CryptoCurrency/app.js
@@ -45,13 +45,20 @@ var limit = "10";
 allCrypto(translate, limit);
 
 function allCrypto(translate, limit) {
+
+  // The layout of the request making it possible to pass two pieces of into into it
   var requestMap = 'https://api.coinmarketcap.com/v1/ticker/?convert=' + translate + '&limit=' + limit;
+
+  // The actual request requesting info from the API
   request(requestMap, function(error, response, body) {
+
+    // Parsing the body into an object
     var info = JSON.parse(body);
 
-    // Reading in current currencies
+    // Reading in the currencydb
     var currencies = jsonfile.readFileSync(currencydb);
 
+    // Everything in here will be done for each entry in the currencydb
     info.forEach(function(entry) {
 
       // Removing previous crypto data from list
@@ -61,7 +68,7 @@ function allCrypto(translate, limit) {
           temp_currencies[temp_currencies.length] = currencies[i];
       currencies = temp_currencies;
 
-      // Adding new crypto data to list
+      // Defining the info which will be put into the currencydb
       var name = entry.name;
       var symbol = entry.symbol;
       var rank = entry.rank;
@@ -70,6 +77,7 @@ function allCrypto(translate, limit) {
       var perc_7d = entry.percent_change_7d;
       var amount = Math.round(entry.price_eur * 100) / 100;
 
+      // Putting the info from above into an object
       currencies = [...currencies, {
         name,
         symbol,
@@ -81,22 +89,28 @@ function allCrypto(translate, limit) {
         translate
       }]
 
-      // Writing out updated currencies
+      // Writing out the updated data into the currencydb (still forEach)
       jsonfile.writeFileSync(currencydb, currencies, {
         spaces: 2
       });
 
     })
 
-    //console.log(crypto + ': ' + info[translate] + " " + translate)
+    // After we put everything into the currencydb, we sort it again
     sortCrypto();
+
   });
 }
 
 function sortCrypto() {
 
+  // Reading in the currencydb
   var currencies = jsonfile.readFileSync(currencydb);
+
+  // Sorting the currencies by their number
   currencies.sort(sortNumber);
+
+  // Writing everything back into the currencydb
   jsonfile.writeFileSync(currencydb, currencies, {
     spaces: 2
   });
@@ -104,54 +118,71 @@ function sortCrypto() {
 }
 
 function sortNumber(a, b) {
+
+  // Actual process of sorting the file
   return b.amount - a.amount;
+
 }
 
 function status() {
 
+  // Getting info from the currencydb
   var currencies = jsonfile.readFileSync(currencydb);
 
+  // Searching the currencydb for "Bitcoin"
   for (var i in currencies) {
     if (currencies[i].name == "Bitcoin") {
+
+      // Setting the bot status to current Bitcoin info
       client.user.setGame(currencies[i].symbol + ' @ ' + currencies[i].amount + '€ (' + currencies[i].perc_24h + '% in last 24h)');
     }
   }
 
-  //client.user.setGame('Write "/crypto" to get started!');
 }
 
 var j = schedule.scheduleJob('00 * * * * *', function() {
-  // Refreshing all crypto information exactly on the minute
-  // in order to sync with the API
+
+  // Refreshing the crypto data every minute
   allCrypto(translate, limit);
+
   // Also updating status at the same time
   status();
 });
 
 client.on('message', msg => {
 
+  // Drop the message if it was sent by the bot itself
   if (msg.author.id == "329331452040708096") {
     return;
   }
 
+  // If the message contains the prefix and the keyword "crypto"
   if (msg.content.toLowerCase() == prefix + "crypto") {
 
-    // Checking server database if there's a special currency set for the server
+    // Reading in the serverdb
     servers = jsonfile.readFileSync(serverdb);
 
+    // Checking server database if there's a special currency set for the server
     for (var i in servers) {
       if (servers[i].serverId == msg.guild.id) {
-        // If there was one found, we're sending a special message
+
+        /*
+            If there was one found, we're sending a special message
+        */
 
         // We take the currency out of the serverdb
         var translate = servers[i].currency_converted;
 
-        // we put the currency to lowercase in order for the api to recognize it
+        // We put the currency to lowercase in order for the api to recognize it
         var translated_currency = "price_" + translate.toLowerCase();
 
+        // The layout of the request making it possible to pass two pieces of into into it
         var requestMap = 'https://api.coinmarketcap.com/v1/ticker/?convert=' + translate + '&limit=' + limit;
+
+        // Starting the inital request
         request(requestMap, function(error, response, body) {
 
+          //
           var info = JSON.parse(body);
           var currencies_special = info;
 
@@ -207,7 +238,10 @@ client.on('message', msg => {
         });
 
       } else {
-        // If there wasn't anything found, we're gonna send a default "EUR" message
+
+        /*
+            If there wasn't anything found, we're gonna send a default "EUR" message
+        */
 
         // Here follows the default process of sending a message
         currencies = jsonfile.readFileSync(currencydb);
